90b1159e3c718438173057baa5af4be08328a05c
asti339 <rahmjor@students.zhaw.ch>
2023-04-26 12:21:26 +0200

    25.04

--- src/main/java/ch/zhaw/freelancer4u/controller/FreelancerController.java
+++ src/main/java/ch/zhaw/freelancer4u/controller/FreelancerController.java
@@ -1,12 +1,10 @@
 package ch.zhaw.freelancer4u.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import ch.zhaw.freelancer4u.model.Freelancer;
 import ch.zhaw.freelancer4u.model.FreelancerCreateDTO;
@@ -40,13 +40,11 @@ public class FreelancerController {
 
     @GetMapping("/freelancer")
     @Secured("ROLE_admin")
-    public ResponseEntity<List<Freelancer>> getAllFreelancer(
-        @RequestParam(required = false,defaultValue = "1") Integer pageNumber,
-            @RequestParam(required = false, defaultValue = "2") Integer pageSize){
-                
-                Pageable pageable = PageRequest.of(pageNumber, pageSize);
-                Page<Freelancer> allFree = freelancerRepository.findAll(pageable);
-                return new ResponseEntity<>(allFree, HttpStatus.OK);
+    public ResponseEntity<Page<Freelancer>> getAllFreelancer(
+            @RequestParam(required = false, defaultValue = "1") Integer pageNumber,
+            @RequestParam(required = false, defaultValue = "2") Integer pageSize) {
+        Page<Freelancer> allFree = freelancerRepository.findAll(PageRequest.of(pageNumber - 1, pageSize));
+        return new ResponseEntity<>(allFree, HttpStatus.OK);
     }
 
     @GetMapping("/freelancer/{id}")
@@ -60,4 +58,13 @@ public class FreelancerController {
         }
     }
 
+    @GetMapping("/me/freelancer")
+    public ResponseEntity<Freelancer> assignToMe(@AuthenticationPrincipal Jwt jwt) {
+        String userEmail = jwt.getClaimAsString("email");
+        Freelancer freelancer = freelancerRepository.findFirstByEmail(userEmail);
+        if (freelancer != null) {
+            return new ResponseEntity<>(freelancer, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/ch/zhaw/freelancer4u/repository/FreelancerRepository.java
+++ src/main/java/ch/zhaw/freelancer4u/repository/FreelancerRepository.java
@@ -1,12 +1,8 @@
 package ch.zhaw.freelancer4u.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ch.zhaw.freelancer4u.model.Freelancer;
 
 public interface FreelancerRepository extends MongoRepository<Freelancer,String>{
-   Page <Freelancer> findFirstByEmail(String email, Pageable pageable);
-
-Freelancer findFirstByEmail(String freelancerEmail, Object pageable);
+    Freelancer findFirstByEmail(String email);
 }
